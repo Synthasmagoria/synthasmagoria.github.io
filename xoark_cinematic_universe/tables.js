1e21d55abbbee3e7801a50311376bb2eee597ffe
Synthasmagoria <Synthasmagoria@users.noreply.github.com>
2026-08-11 22:33:08 +0000

    deploy: fdfbb115cc24b11538fd6e2eacd7714cc5100e6f

--- xoark_cinematic_universe/tables.js
+++ xoark_cinematic_universe/tables.js
@@ -1,28 +1,148 @@
-const tables = new Map();
-for (let i = 0; i < window.xoark_database.length; i++) {
-    const item = window.xoark_database[i];
-    if (!tables.has(item.series)) {
-        tables.set(item.series, []);
-    }
-    item.released_date = new Date(item.released);
-    tables.get(item.series).push(item);
-}
-const iter = tables.values();
-const sortFn = function (a, b) { return a.released_date < b.released_date ? -1 : 1; };
-for (let item = iter.next(); item.value != undefined; item = iter.next()) {
-    item.value.sort(sortFn);
-}
-window.xoark_tables = tables;
+const Genre = Object.freeze({
+    IDM: 0,
+    Noise: 1,
+    Glitch: 2,
+    Breakcore: 3,
+    SoCalledDnb: 4,
+    Ambient: 5,
+    Placeholder01: 6,
+    Placeholder02: 7,
+    Placeholder03: 8,
+    Placeholder04: 9,
+    Placeholder05: 10,
+    Placeholder06: 11,
+    Placeholder07: 12,
+    Placeholder08: 13,
+    Placeholder09: 14,
+    Placeholder10: 15,
+    Placeholder11: 16,
+    Placeholder12: 17,
+    Placeholder13: 18,
+    Placeholder14: 19,
+    Placeholder15: 20,
+    Placeholder16: 21,
+});
 
-function createAndInsertTable(targetId, series) {
+const Type = Object.freeze({
+    Album: 0,
+    EP: 1,
+    Split: 2,
+    VA: 3,
+    Journey: 4,
+    Single: 5,
+});
+
+const Series = Object.freeze({
+    None: 0,
+    Hex: 1,
+    Base64: 2,
+    P: 3,
+    Missing_Broken: 4,
+    Satellits: 5,
+    Praspis: 6,
+    Gnocchi: 7,
+    Runtime: 8,
+    _Count: 9,
+});
+
+class Date {
+    constructor(year, month, day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+}
+
+class Time {
+    constructor(hours, minutes, seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+}
+
+function viewGetDate(view, cursor) {
+    return new Date(view.getUint16(cursor, true), view.getUint8(cursor + 2), view.getUint8(cursor + 3))
+}
+
+function viewGetTime(view, cursor) {
+    return new Time(view.getUint8(cursor), view.getUint8(cursor + 1), view.getUint8(cursor + 2));
+}
+
+function viewGetString(view, cursor) {
+    const length = view.getUint16(cursor, true);
+    const decoder = new TextDecoder("utf-8");
+    const array = new Uint8Array(view.buffer, cursor + 2, length);
+    return { string: decoder.decode(array), bytes: 2 + length };
+}
+
+class Album {
+	genre = 0;                // bit_set[Genre],
+	type = 0;                 // Type,
+	series = 0;               // Series,
+	rating = 0;               // u8,
+	listened = null;          // dt.Date,
+	released = null;          // dt.Date,
+	duration = null;          // dt.Time,
+	title = "";               // string,
+	art = "";                 // string,
+	url = "";                 // string,
+	favorite = "";            // string,
+	comment = "";             // string,
+	listens = 0;              // i32,
+}
+
+function viewReadAlbum(view, cursor) {
+    const album = new Album();
+    album.genre = view.getUint32(cursor, true);
+    cursor += 4;
+    album.type = view.getUint8(cursor);
+    cursor += 1;
+    album.series = view.getUint8(cursor);
+    cursor += 1;
+    album.rating = view.getUint8(cursor);
+    cursor += 1
+    album.listened = viewGetDate(view, cursor);
+    cursor += 4;
+    album.released = viewGetDate(view, cursor);
+    cursor += 4;
+    album.duration = viewGetTime(view, cursor);
+    cursor += 3;
+    {
+        const result = viewGetString(view, cursor);
+        album.title = result.string;
+        cursor += result.bytes;
+    }
+    {
+        const result = viewGetString(view, cursor);
+        album.art = result.string;
+        cursor += result.bytes;
+    }
+    {
+        const result = viewGetString(view, cursor);
+        album.url = result.string;
+        cursor += result.bytes;
+    }
+    {
+        const result = viewGetString(view, cursor);
+        album.favorite = result.string;
+        cursor += result.bytes;
+    }
+    {
+        const result = viewGetString(view, cursor);
+        album.comment = result.string;
+        cursor += result.bytes;
+    }
+    album.listens = view.getInt32(view, cursor);
+    cursor += 4;
+
+    return { album: album, cursor: cursor };
+}
+
+function createAndInsertTable(targetId, items) {
     const element = document.getElementById(targetId);
     if (element === undefined) {
         console.error(targetId + " did not point to a valid element id");
-        return;
-    }
-    const items = window.xoark_tables.get(series);
-    if (items === undefined) {
-        console.error(series + " was not a valid xoark series");
         return;
     }
 
@@ -55,3 +175,25 @@ function createAndInsertTable(targetId, series) {
 </table>`);
     element.innerHTML = html.join("");
 }
+
+fetch("/xoark_db.bin").then((response) => {
+    if (!response.ok) {
+        throw new Error(response.url + " response was not ok");
+    }
+    response.arrayBuffer().then((arrayBuffer) => {
+        const view = new DataView(arrayBuffer);
+        let cursor = 0;
+        const albums = new Array(Series._Count);
+        for (let i = 0; i < albums.length; i++) {
+            albums[i] = [];
+        }
+        while (cursor < view.byteLength) {
+            const result = viewReadAlbum(view, cursor);
+            cursor = result.cursor;
+            albums[result.album.series].push(result.album);
+        }
+        console.log(albums[Series.Satellits].length);
+        createAndInsertTable("satellits", albums[Series.Satellits]);
+        createAndInsertTable("hex", albums[Series.Hex]);
+    });
+});
